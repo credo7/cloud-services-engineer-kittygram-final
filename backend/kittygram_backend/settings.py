a92d9c0ab46d77ab9fa365843a0d896dad793705
credo7 <ah.vit@mail.ru>
2025-01-10 13:44:07 +0300

    fix admin

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -93,8 +93,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static", "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
