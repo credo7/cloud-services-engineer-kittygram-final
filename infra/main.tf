6e4be9223dea2f785ca968e6890c6597ef8d5c56
credo7 <vitaly.credo@gmail.com>
2025-04-20 21:09:25 +0300

    fix terraform

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,6 @@ terraform {
       source  = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
 
   backend "s3" {
     endpoint                     = "https://storage.yandexcloud.net"
